7175ff45357ab71b26deb94677f39b84fd22d9c9
asaniDev <asanidev9@gmail.com>
2025-12-06 14:40:40 +0000

    created a card div and added an episode name to it

--- script.js
+++ script.js
@@ -7,6 +7,20 @@ function setup() {
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+
+  const card = document.createElement("div");
+
+  const episodeName = document.createElement("h1");
+  episodeName.textContent =
+    episodeList[0].name +
+    " - " +
+    "S" +
+    episodeList[0].season.toString().padStart(2, "0") +
+    "E" +
+    episodeList[0].number.toString().padStart(2, "0");
+
+  card.appendChild(episodeName);
+  rootElem.appendChild(card);
 }
 
 window.onload = setup;
